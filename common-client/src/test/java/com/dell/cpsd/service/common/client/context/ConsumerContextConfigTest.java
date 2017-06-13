b7232487c0f4f0b6f697383922983bc7caf2548c
boettj3 <James.Boettcher@dell.com>
2017-06-13 15:11:11 -0400

    ESTS-127994 add more unit tests

---
+++ common-client/src/test/java/com/dell/cpsd/service/common/client/context/ConsumerContextConfigTest.java
@@ -0,0 +1,67 @@
+package com.dell.cpsd.service.common.client.context;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+/**
+ * TODO: Document usage.
+ * <p>
+ * <p>
+ * Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
+ * Dell EMC Confidential/Proprietary Information
+ * </p>
+ */
+public class ConsumerContextConfigTest
+{
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @Test
+    public void consumerName() throws Exception
+    {
+        String name = "test name";
+        ConsumerContextConfig configToTest = new ConsumerContextConfig(name, true);
+
+        assertEquals(name, configToTest.consumerName());
+    }
+
+    @Test
+    public void consumerUuid()
+    {
+        String name = "test name";
+        ConsumerContextConfig configToTest = new ConsumerContextConfig(name, true);
+
+        String containerId = configToTest.consumerUuid();
+
+        String[] containerParts = containerId.split("\\.");
+        String uuid = containerParts[2];
+        assertTrue(containerParts.length == 3);
+        assertEquals(containerParts[0], name);
+        try
+        {
+            UUID uuid1 = UUID.fromString(uuid);
+        }
+        catch (IllegalArgumentException ex)
+        {
+            fail("Returned value does not contain a valid UUID");
+        }
+
+    }
+
+    @Test
+    public void stateful() throws Exception
+    {
+        String name = "test name";
+        boolean stateful = false;
+        ConsumerContextConfig configToTest = new ConsumerContextConfig(name, stateful);
+
+        assertFalse(configToTest.stateful());
+    }
+
+}
