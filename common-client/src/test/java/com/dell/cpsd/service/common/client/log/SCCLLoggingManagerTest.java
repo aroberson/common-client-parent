2747dbe90f68e02d57b2d781dfb22457b684fb65
boettj3 <James.Boettcher@dell.com>
2017-06-14 09:59:08 -0400

    ESTS-127994 add more unit tests

---
+++ common-client/src/test/java/com/dell/cpsd/service/common/client/log/SCCLLoggingManagerTest.java
@@ -0,0 +1,33 @@
+package com.dell.cpsd.service.common.client.log;
+
+import com.dell.cpsd.common.logging.DelegateLogger;
+import com.dell.cpsd.common.logging.ILogger;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * TODO: Document usage.
+ * <p>
+ * <p>
+ * Copyright &copy; 2017 Dell Inc. or its subsidiaries.  All Rights Reserved.
+ * Dell EMC Confidential/Proprietary Information
+ * </p>
+ */
+public class SCCLLoggingManagerTest
+{
+    @Test
+    public void getLogger() throws Exception
+    {
+        ILogger logger = SCCLLoggingManager.getLogger(SCCLLoggingManagerTest.class);
+        assertTrue(logger instanceof DelegateLogger);
+    }
+
+    @Test
+    public void getLogger1() throws Exception
+    {
+        ILogger logger = SCCLLoggingManager.getLogger("test");
+        assertTrue(logger instanceof DelegateLogger);
+    }
+
+}
